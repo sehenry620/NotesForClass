4a7ed21b44fbf9eb4b8f36f46722703bce323ec0
Henry <henrstep@amazon.com>
2019-09-19 11:19:30 -0400

    update notes

---
+++ assets/notes.js
@@ -0,0 +1,36 @@
+// function catString(aString){
+//     return "anything" + aString + "4";
+// }
+
+// let myArray = ["dog", "cat", "frog"]
+    
+// for (let i = 0; i < myArray.length; i++){
+//     console.log(catString(myArray[i]));
+// }
+
+
+// function cramerDrink(aString){
+//     return "cramer" + " " +"likes" + " " + "to" + " " + "burp";
+// }
+
+// let secondArray = ["Moutain Dew", "Pepsi", "Diet Coke"]
+
+// for (i = 0; i < secondArray.length; i++){
+//     console.log(cramerDrink(secondArray[i]));
+// }
+
+// function someFunction(potato){ // "potato" is used a variable
+//     console.log(potato+"!!")
+// }
+
+// someFunction("goodbye")
+// someFunction("hello")
+
+function someFunction (potato, tomato, something){
+    console.log (potato +"!!!")
+    console.log (tomato)
+    console.log (something + "?")
+}
+
+someFunction ("a", "b", "c")
+someFunction ()
