4452921796ae71af1a8783d9fa99d80c2d385fd5
Henry <henrstep@amazon.com>
2019-09-19 11:38:52 -0400

    update notes

--- assets/notes.js
+++ assets/notes.js
@@ -26,11 +26,24 @@
 // someFunction("goodbye")
 // someFunction("hello")
 
-function someFunction (potato, tomato, something){
-    console.log (potato +"!!!")
-    console.log (tomato)
-    console.log (something + "?")
+// function someFunction (potato, tomato, something){
+//     console.log (potato +"!!!")
+//     console.log (tomato)
+//     console.log (something + "?")
+// }
+
+// someFunction ("a", "b", "c")
+// someFunction ()
+
+function findMaxNumber(numberArray){
+    return Math.max(...numberArray);
 }
 
-someFunction ("a", "b", "c")
-someFunction ()
+let firstMaxNum = findMaxNumber([2, 500, 489, 345])
+let secondMaxNum = findMaxNumber([34, 500, 673, 20])
+let thirdMaxNum = findMaxNumber([1000, 342, 5000, 456])
+
+console.log(firstMaxNum)
+console.log(secondMaxNum)
+console.log(thirdMaxNum)
+// console.log(findMaxNumber([100, 2376, 4893, 894]))
